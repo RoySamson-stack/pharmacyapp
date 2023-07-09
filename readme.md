7defc5359ab60fd583e195ee71a08f417210231a
EuniceKiingati <47145776+EuniceKiingati@users.noreply.github.com>
2023-07-09 20:45:01 +0300

    added back to home button and readme file

---
+++ readme.md
@@ -0,0 +1,39 @@
+# Online Pharmacy App 
+
+This is an ios app built using react-native and firebase. At the moment, it only has the client side, but the long term plan is to include other users like the admin, and partners. 
+
+## Features
+The system main menu prompts the user to choose any of the following:
+1. Login, SignUp, Signout
+2. View products and services
+3. Add-to-Cart
+4. Entering address details and proceeding to checkout
+
+## Screens
+- Login screen
+- Signup Screen
+- Profile Screen
+- Homescreen
+- Cart Screen
+- Pickup Screen
+- Order Screen
+
+
+
+
+
+
+
+
+
+
+### How to run this app
+1.Install git
+
+2.Clone the [repo](https://github.com/EuniceKiingati/.git)
+
+3. To install all the dependencies, use node package manager and run the command
+
+4.Run the project using  "npx expo start" .Runs your app in development mode.
+
+
--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -1,8 +1,12 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native";
 import React from "react";
 import LottieView from "lottie-react-native";
+import { useNavigation } from "@react-navigation/native";
+
 
 const OrderScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView>
       <LottieView
@@ -43,6 +47,18 @@ const OrderScreen = () => {
             
 
       </LottieView>
+
+      <View>
+      <Pressable onPress={() => navigation.navigate("Home")}
+          style={{ marginTop: 20 }}>
+                <Text style={{
+                textAlign: "center",
+                fontSize: 17,
+                color: "gray",
+                fontWeight: "500",
+              }}>Back to Homescreen</Text>
+            </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
